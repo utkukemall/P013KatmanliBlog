30c6be2c5548b9f7a82bb7999862caf4e2108662
utkukemall <utkukemal1999@gmail.com>
2023-06-03 14:30:53 +0300

    Gorev14Bitti

--- P013KatmanliBlog.Core/Entities/AppUser.cs
+++ P013KatmanliBlog.Core/Entities/AppUser.cs
@@ -5,11 +5,13 @@ namespace P013KatmanliBlog.Core.Entities
     public class AppUser : IEntity
     {
         public int Id { get; set; }
-        [Display(Name = "Ad")]
+        [Display(Name = "Ad"), Required(ErrorMessage ="{0} Alanı Gereklidir!")]
         public string Name { get; set; }
         [Display(Name = "Soyad")]
         public string? Surname { get; set; }
+        [Required(ErrorMessage = "{0} Alanı Gereklidir!")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "{0} Alanı Gereklidir!")]
         [Display(Name = "Şifre")]
         public string Password { get; set; }
         [Display(Name = "Kullanıcı Adı")]
--- P013KatmanliBlog.MVCUI/Program.cs
+++ P013KatmanliBlog.MVCUI/Program.cs
@@ -8,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSession();
+
 builder.Services.AddDbContext<DatabaseContext>();
 
 builder.Services.AddTransient(typeof(IService<>), typeof(Service<>));
@@ -37,6 +39,8 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseSession();
 
 app.UseAuthentication();
 app.UseAuthorization();
